ed934668d8a210f04abf9a68a443a89ba4ec9c25
Abdulhay Muslim <amu-coding@users.noreply.github.com>
2025-08-08 18:51:57 +0430

    js file created for home page to add products dynamically and add functionality

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Products that are going to be added dynamically in my website
+const products = [
+    {
+        id: 1,
+        name: "Laptop",
+        model: "MacBook Air M4",
+        desc: "Brand new MacBook Air M4 (2025), light weight, fast and portable laptop.",
+        price: 1200,
+        image: "../uploads/laptop (6).jpg"
+    }
+];
